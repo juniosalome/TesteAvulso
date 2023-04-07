b18bcb09209a96acaaa0b8d1f027b9d20d520ec9
juniosalome <junio.salome@gmail.com>
2023-04-07 09:41:18 -0300

    Update Program04.java

--- java/dio/Program04.java
+++ java/dio/Program04.java
@@ -1,1 +1,32 @@
+/*
+A prefeitura de uma cidade A instalou uma câmera para verificar quem 
+estava acima da velocidade permitida na rodovia da entrada da cidade. 
+Porém, ela é uma cidade extremamente movimentada e a prefeitura não 
+deu conta de enviar as multas necessárias para quem ultrapassasse a 
+velocidade permitida de 60km/h.
+O seu papel será o de criar um programa que verifique os dados 
+recebidos da câmera e envie uma mensagem dizendo se o motorista será multado ou não.
 
+*/
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:  
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next"; 
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.   
+
+import java.util.Scanner; 
+    
+public class Program04 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int velocidadeAtual = leitor.nextInt();
+        
+        // TODO: Crie a condição necessária para verificar se o motorista, de acordo com a entrada, foi multado ou não.
+
+        if(velocidadeAtual <= 60){
+          System.out.println("Nao foi multado");
+        }
+        else{
+          System.out.println("Foi multado");
+        }
+       
+    }
+}
